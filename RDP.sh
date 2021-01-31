126907d36a3ac8b1f706fbb7a4a61b88a54ef5d8
HeroineS2 <77723466+HeroineS2@users.noreply.github.com>
2021-01-31 09:17:01 +0700

    Update RDP.sh

--- RDP.sh
+++ RDP.sh
@@ -14,7 +14,8 @@ apt install --assume-yes xfce4 desktop-base
 sudo bash -c 'echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" > /etc/chrome-remote-desktop-session'  
 sudo apt install --assume-yes xscreensaver
 sudo systemctl disable lightdm.service
-sudo apt install firefox
+wget https://download-installer.cdn.mozilla.net/pub/firefox/releases/85.0/linux-x86_64/vi/firefox-85.0.tar.bz2
+tar xjf firefox-*.tar.bz2
 sudo apt install --assume-yes --fix-broken
 sudo apt install nautilus nano -y 
 sudo adduser ALOK chrome-remote-desktop
